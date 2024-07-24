2fa12d671b6617fd78e358d1b7e1903057cde3ac
Daniel <guardians01@outlook.com>
2024-07-23 18:12:19 -0600

    Ya registra medicamento en alta medicamento (Faltan validaciones).

--- src/components/routes/new/AltaMedicamento.jsx
+++ src/components/routes/new/AltaMedicamento.jsx
@@ -48,12 +48,56 @@ export const AltaMedicamento = () => {
             console.error('Error al obtener las sustancias:', error);
         });
     }, []);
+    const handleAltaMedicamento = async (event) => {
+        event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+        // Obtener los valores del formulario
+        const nombre = document.getElementById('name').value;
+        const sustancia = document.getElementById('substance').value;
+        const presentacion = document.getElementById('presentation').value;
+        const laboratorio = document.getElementById('laboratory').value;
+        const descripcion = document.getElementById('description').value;
+        const indicaciones = document.getElementById('indications').value;
+        const imagen = document.getElementById('image').files[0]; // Manejar la subida de archivos según necesidad
+        console.log(nombre,
+            sustancia,
+            presentacion,
+            laboratorio,
+            descripcion,
+            indicaciones,
+            imagen,);
+
+        // Crear un objeto con los datos del formulario
+        const formData = new FormData();
+        formData.append('nombre', nombre);
+        formData.append('sustancia', sustancia);
+        formData.append('presentacion', presentacion);
+        formData.append('laboratorio', laboratorio);
+        formData.append('descripcion', descripcion);
+        formData.append('indicaciones', indicaciones);
+        formData.append('imagen', imagen);
+        console.log(formData);
+
+        // Enviar los datos al servidor
+        try {
+            const response = await fetch('http://localhost:3001/api/events/new/medicamento/', {
+                method: 'POST',
+                body: formData
+            });
+            const data = await response.json();
+            console.log('Respuesta del servidor:', data);
+            // Aquí puedes manejar la respuesta del servidor, por ejemplo, mostrar un mensaje de éxito al usuario
+        } catch (error) {
+            console.error('Error al enviar los datos:', error);
+            // Aquí puedes manejar errores, como mostrar un mensaje al usuario indicando que hubo un problema
+        }
+    };
 
     return (
         <div className="container mt-5 mb-5">
             <div className="bg-form">
                 <h2 className="font-title d-flex justify-content-center mb-5">Nuevo Medicamento</h2>
-                <form className="row">
+                <form className="row" onSubmit={handleAltaMedicamento}>
                     <div className="col-12 col-md-6 mb-3">
                         <label htmlFor="name" className="font-form form-label">Nombre</label>
                         <input type="text" id="name" className="form-control" placeholder="Ingresa el nombre del medicamento" />
@@ -85,7 +129,7 @@ export const AltaMedicamento = () => {
                         <select id="laboratory" className="font-ddl form-select">
                             <option value="">Selecciona el laboratorio</option>
                             {laboratorios.map(lab => (
-                                <option key={lab.id} value={lab.id}>
+                                <option key={lab.id} value={lab.nombre}>
                                     {lab.nombre}
                                 </option>
                             ))}
